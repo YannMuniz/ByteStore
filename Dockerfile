cde1d6088eee397ff864073f11f21e8bc494c020
Yann Muniz <yannmunizbarbosa@gmail.com>
2026-05-25 11:35:00 -0300

    Tailwind configurado

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM php:8.3-fpm
 
 RUN apt-get update && apt-get install -y \
-    git curl zip unzip libpq-dev \
-    && docker-php-ext-install pdo pdo_mysql pdo_pgsql
+    git curl zip unzip libpq-dev libonig-dev libxml2-dev \
+    && docker-php-ext-install pdo pdo_mysql mbstring exif pcntl bcmath gd
 
 COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
 
@@ -18,6 +18,11 @@ RUN composer install --optimize-autoloader && \
     chmod -R 777 storage bootstrap/cache && \
     if [ ! -f .env ]; then cp .env.example .env; fi && \
     php artisan key:generate --force
+
+RUN npm install
+RUN npm install tailwindcss @tailwindcss/vite
+
+RUN chown -R www-data:www-data /var/www
 
 EXPOSE 9000
 CMD ["php-fpm"]
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'localhost',
+            host: '127.0.0.1',
             port: 5173,
+            protocol: 'http',
         },
     },
-
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js'
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
-});
+})
